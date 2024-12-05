5a2afa4b649c3911b7928b11e4c152f9edcb29c2
Benjamin Miller <benhmiller22@gmail.com>
2024-12-05 16:37:41 -0600

    Continued file cleaning

--- pdf_extraction/pdf_comparison_driver.py
+++ pdf_extraction/pdf_comparison_driver.py
@@ -1,11 +1,24 @@
-
-
+from documentai_handler import documentai_process_pdf
+'''
+File to run and compare PDF table extraction performed by Google Document AI, camelot, and gmft
+'''
 if __name__ == '__main__':
     # Begin Input Loop
     user_input = input('Enter file name (enter \'q\' to quit): ')
     while user_input.lower() != "q":
+        # Validate File Name
         file_path = f'../classification_model/loss_runs/input/{user_input}'
         print(file_path)
+
+        # Call PDF Processors
+        camelot_tables = camelot_process_pdf(file_path)
+        gmft_tables = gmft_process_pdf(file_path)
+
+        # Compare Outputs
+        # ...
+
+        # Document AI Fallback (saves API Calls)
+        #docai_tables = documentai_process_pdf(file_path)
         
         # Continue input loop
         user_input = input('Enter file name (enter \'q\' to quit): ')
